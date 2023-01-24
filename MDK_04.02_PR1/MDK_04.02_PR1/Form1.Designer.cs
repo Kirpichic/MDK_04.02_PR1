b140f028d5e5dccc8e276c532cd297b21ed4529b
Mighty kitten <81363730+Kirpichic@users.noreply.github.com>
2023-01-24 03:18:15 +0300

    4

--- MDK_04.02_PR1/MDK_04.02_PR1/Form1.Designer.cs
+++ MDK_04.02_PR1/MDK_04.02_PR1/Form1.Designer.cs
@@ -32,19 +32,33 @@ namespace MDK_04._02_PR1
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Линейный = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Otwet = new System.Windows.Forms.RichTextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.textBoxX = new System.Windows.Forms.TextBox();
-            this.textBoxY = new System.Windows.Forms.TextBox();
-            this.textBoxZ = new System.Windows.Forms.TextBox();
-            this.Otwet = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.otvet2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BoxX = new System.Windows.Forms.TextBox();
+            this.BoxY = new System.Windows.Forms.TextBox();
+            this.f1 = new System.Windows.Forms.RadioButton();
+            this.f2 = new System.Windows.Forms.RadioButton();
+            this.f3 = new System.Windows.Forms.RadioButton();
             this.Линейный.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Линейный
@@ -54,7 +68,7 @@ namespace MDK_04._02_PR1
             this.Линейный.Location = new System.Drawing.Point(3, 1);
             this.Линейный.Name = "Линейный";
             this.Линейный.SelectedIndex = 0;
-            this.Линейный.Size = new System.Drawing.Size(465, 611);
+            this.Линейный.Size = new System.Drawing.Size(461, 611);
             this.Линейный.TabIndex = 0;
             // 
             // tabPage1
@@ -71,10 +85,49 @@ namespace MDK_04._02_PR1
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(457, 585);
+            this.tabPage1.Size = new System.Drawing.Size(453, 585);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Линейный";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Otwet
+            // 
+            this.Otwet.Location = new System.Drawing.Point(19, 321);
+            this.Otwet.Name = "Otwet";
+            this.Otwet.Size = new System.Drawing.Size(409, 177);
+            this.Otwet.TabIndex = 7;
+            this.Otwet.Text = "";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(243, 261);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(185, 20);
+            this.textBoxZ.TabIndex = 6;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(243, 214);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(185, 20);
+            this.textBoxY.TabIndex = 5;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(243, 169);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(185, 20);
+            this.textBoxX.TabIndex = 4;
             // 
             // label3
             // 
@@ -119,60 +172,132 @@ namespace MDK_04._02_PR1
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.BoxY);
+            this.tabPage2.Controls.Add(this.BoxX);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.otvet2);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.pictureBox2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Size = new System.Drawing.Size(457, 618);
+            this.tabPage2.Size = new System.Drawing.Size(453, 585);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Разветвляющийся";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // textBoxX
+            // pictureBox2
             // 
-            this.textBoxX.Location = new System.Drawing.Point(243, 169);
-            this.textBoxX.Name = "textBoxX";
-            this.textBoxX.Size = new System.Drawing.Size(185, 20);
-            this.textBoxX.TabIndex = 4;
-            this.textBoxX.TextChanged += new System.EventHandler(this.textBoxX_TextChanged);
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(444, 165);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
             // 
-            // textBoxY
+            // label4
             // 
-            this.textBoxY.Location = new System.Drawing.Point(243, 214);
-            this.textBoxY.Name = "textBoxY";
-            this.textBoxY.Size = new System.Drawing.Size(185, 20);
-            this.textBoxY.TabIndex = 5;
-            this.textBoxY.TextChanged += new System.EventHandler(this.textBoxY_TextChanged);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "X = ";
             // 
-            // textBoxZ
+            // label5
             // 
-            this.textBoxZ.Location = new System.Drawing.Point(243, 261);
-            this.textBoxZ.Name = "textBoxZ";
-            this.textBoxZ.Size = new System.Drawing.Size(185, 20);
-            this.textBoxZ.TabIndex = 6;
-            this.textBoxZ.TextChanged += new System.EventHandler(this.textBoxZ_TextChanged);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(16, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 25);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Y = ";
             // 
-            // Otwet
+            // groupBox1
             // 
-            this.Otwet.Location = new System.Drawing.Point(19, 321);
-            this.Otwet.Name = "Otwet";
-            this.Otwet.Size = new System.Drawing.Size(409, 177);
-            this.Otwet.TabIndex = 7;
-            this.Otwet.Text = "";
+            this.groupBox1.Controls.Add(this.f3);
+            this.groupBox1.Controls.Add(this.f2);
+            this.groupBox1.Controls.Add(this.f1);
+            this.groupBox1.Location = new System.Drawing.Point(197, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 173);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор функции";
             // 
-            // button1
+            // otvet2
             // 
-            this.button1.Location = new System.Drawing.Point(184, 534);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Ответ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.otvet2.Location = new System.Drawing.Point(5, 353);
+            this.otvet2.Name = "otvet2";
+            this.otvet2.Size = new System.Drawing.Size(442, 177);
+            this.otvet2.TabIndex = 8;
+            this.otvet2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 536);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ответ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BoxX
+            // 
+            this.BoxX.Location = new System.Drawing.Point(72, 231);
+            this.BoxX.Name = "BoxX";
+            this.BoxX.Size = new System.Drawing.Size(119, 20);
+            this.BoxX.TabIndex = 10;
+            // 
+            // BoxY
+            // 
+            this.BoxY.Location = new System.Drawing.Point(72, 268);
+            this.BoxY.Name = "BoxY";
+            this.BoxY.Size = new System.Drawing.Size(119, 20);
+            this.BoxY.TabIndex = 11;
+            // 
+            // f1
+            // 
+            this.f1.AutoSize = true;
+            this.f1.Location = new System.Drawing.Point(78, 37);
+            this.f1.Name = "f1";
+            this.f1.Size = new System.Drawing.Size(109, 17);
+            this.f1.TabIndex = 0;
+            this.f1.TabStop = true;
+            this.f1.Text = "Первая функция";
+            this.f1.UseVisualStyleBackColor = true;
+            // 
+            // f2
+            // 
+            this.f2.AutoSize = true;
+            this.f2.Location = new System.Drawing.Point(78, 74);
+            this.f2.Name = "f2";
+            this.f2.Size = new System.Drawing.Size(107, 17);
+            this.f2.TabIndex = 1;
+            this.f2.TabStop = true;
+            this.f2.Text = "Вторая функция";
+            this.f2.UseVisualStyleBackColor = true;
+            // 
+            // f3
+            // 
+            this.f3.AutoSize = true;
+            this.f3.Location = new System.Drawing.Point(78, 112);
+            this.f3.Name = "f3";
+            this.f3.Size = new System.Drawing.Size(107, 17);
+            this.f3.TabIndex = 2;
+            this.f3.TabStop = true;
+            this.f3.Text = "Третья функция";
+            this.f3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 616);
+            this.ClientSize = new System.Drawing.Size(465, 616);
             this.Controls.Add(this.Линейный);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -180,6 +305,11 @@ namespace MDK_04._02_PR1
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -198,6 +328,17 @@ namespace MDK_04._02_PR1
         private System.Windows.Forms.TextBox textBoxY;
         private System.Windows.Forms.RichTextBox Otwet;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox BoxY;
+        private System.Windows.Forms.TextBox BoxX;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox otvet2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton f3;
+        private System.Windows.Forms.RadioButton f2;
+        private System.Windows.Forms.RadioButton f1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- MDK_04.02_PR1/MDK_04.02_PR1/Form1.cs
+++ MDK_04.02_PR1/MDK_04.02_PR1/Form1.cs
@@ -12,42 +12,33 @@ namespace MDK_04._02_PR1
 {
     public partial class Form1 : Form
     {
-        /*double V = 0;*/
         double x, y, z;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToDouble(BoxX.Text); y = Convert.ToDouble(BoxY.Text);
+            if (f1.Checked == true) {
+
+                /*otvet2.Text = Convert.ToString();*/
+
+            } else if (f2.Checked == true) { 
+            
+            } else if (f3.Checked == true)
+            {
+
+            }
+        }
 
         public Form1()
         {
             InitializeComponent();
-            /*textBoxX.Text = Convert.ToString(V);
-            textBoxY.Text = Convert.ToString(V);
-            textBoxZ.Text = Convert.ToString(V);*/
-        }
-
-        private void textBoxX_TextChanged(object sender, EventArgs e)
-        {
-            /*x = Convert.ToDouble(textBoxX.Text); y = Convert.ToDouble(textBoxY.Text); z = Convert.ToDouble(textBoxZ.Text);
-            xyz(x, y, z);*/
-        }
-
-        private void textBoxZ_TextChanged(object sender, EventArgs e)
-        {
-            /*xyz(Convert.ToDouble(textBoxX.Text), Convert.ToDouble(textBoxY.Text), Convert.ToDouble(textBoxZ.Text));*/
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-                x = Convert.ToDouble(textBoxX.Text); y = Convert.ToDouble(textBoxY.Text); z = Convert.ToDouble(textBoxZ.Text);
-                Otwet.Text = Convert.ToString(((2 * Math.Cos(x - (Math.PI / 6))) / (0.5 + (Math.Sin(y) * Math.Sin(y)))) * (1 + ((z * z) / (3 - ((z * z) / 5)))));
+            x = Convert.ToDouble(textBoxX.Text); y = Convert.ToDouble(textBoxY.Text); z = Convert.ToDouble(textBoxZ.Text);
+            Otwet.Text = Convert.ToString(((2 * Math.Cos(x - (Math.PI / 6))) / (0.5 + (Math.Sin(y) * Math.Sin(y)))) * (1 + ((z * z) / (3 - ((z * z) / 5)))));
         }
-
-        private void textBoxY_TextChanged(object sender, EventArgs e)
-        {
-            /*xyz(Convert.ToDouble(textBoxX.Text), Convert.ToDouble(textBoxY.Text), Convert.ToDouble(textBoxZ.Text));*/
-        }
-
-        /*void xyz(double x, double y, double z)
-        {
-            Otwet.Text = Convert.ToString(((2*Math.Cos(x-(Math.PI/6)))/(0.5+(Math.Sin(y)*Math.Sin(y))))*(1+((z*z)/(3-((z*z)/5)))));
-        }*/
     }
 }
